f3e279224ab07bc891979c65599470c807711445
NVelichkovski <velickovski.n@yahoo.com>
2020-02-02 20:50:14 +0100

    Recording capability to the EnvRenderer

--- env_renderer.py
+++ env_renderer.py
@@ -1,25 +1,57 @@
-# 42:36
-
-from PIL import Image
 import cv2
 import numpy as np
-from time import sleep
-from matplotlib import pyplot as plt
+import os
+from datetime import datetime
 
-from environment_generator import Environment
+from PIL import Image
 
 
 class EnvRenderer:
-    def __init__(self, env: Environment):
+    def __init__(self, env, delay=50):
         self.env = env
-        self.img = None
+        self.images = []
+        self.delay = delay
 
-    def render(self):
-        self.img = Image.fromarray(self.env.matrix, 'RGB')
-        self.img = self.img.resize((500, 500), Image.NONE)
+    def record(self, img=None):
+        img = self.generate_img() if img is None else img
+        self.images.append(img)
+
+    def generate_img(self):
+        self.image = Image.fromarray(self.env.matrix, 'RGB')
+        self.image = self.image.resize((500, 500), Image.NONE)
+        return self.image
+
+    def render(self, record=True):
+        img = self.generate_img()
+
         cv2.namedWindow('SnakeAI', cv2.WINDOW_NORMAL)
-        cv2.imshow('SnakeAI', np.array(self.img))
-        cv2.waitKey(delay=100)
+        cv2.imshow('SnakeAI', np.array(img))
+        cv2.waitKey(delay=self.delay)
 
-    def destry_window(self):
+        if record:
+            self.images.append(img)
+
+    def destroy_window(self):
         cv2.destroyWindow('SnakeAI')
+
+    def save_images(self, path='./game_images'):
+        if len(self.images) == 0:
+            return
+        if not os.path.exists(path):
+            os.mkdir(path)
+        path = path + '/' + str(datetime.now()).replace(' ', '').replace('.', '').replace('-', '').replace(':', '')
+        if not os.path.exists(path):
+            os.mkdir(path)
+        for i, img in enumerate(self.images):
+            cv2.imwrite(f"{str(path)}/{str(i)}.png", np.array(img))
+
+    def save_video(self, path='./game_videos'):
+        if len(self.images) == 0:
+            return
+        if not os.path.exists(path):
+            os.mkdir(path)
+        filename = str(datetime.now()).replace(' ', '').replace('.', '').replace('-', '').replace(':', '')
+        out = cv2.VideoWriter(f'{path}/v{filename}.avi', cv2.VideoWriter_fourcc(*'DIVX'), 20, (500, 500))
+        for img in self.images:
+            out.write(np.array(img))
+        out.release()
