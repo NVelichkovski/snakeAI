3c8766ada202f030dcb376779f423025bdc5ca2f
NVelichkovski <velickovski.n@yahoo.com>
2020-05-12 02:31:50 +0200

    New Examples: Simple DQN

--- examples/simple_dqn.py
+++ examples/simple_dqn.py
@@ -1,7 +1,12 @@
 import tensorflow as tf
 import tensorflow.keras as k
 
+from Snake.env_renderer import CV2Renderer
 from Snake.environment import SnakeMaze
+
+import numpy as np
+
+from Snake.variables import Status
 
 
 class DQN(k.Model):
@@ -54,10 +59,92 @@ class DQN(k.Model):
         return self.out(x)
 
 
+optimizer = tf.keras.optimizers.RMSprop()
+model = DQN(input_shape=(52, 52))
+
+
+@tf.function
+def train_step(state, target):
+    with tf.GradientTape() as tape:
+        Q = model(state, training=True)
+        l = loss(Q, target)
+    grad = tape.gradient(l, model.trainable_variables)
+    optimizer.apply_gradients(zip(grad, model.trainable_variables))
+
+
+@tf.function
+def loss(p, t):
+    return tf.reduce_sum(tf.square(t - p))
+
+
+num_step_per_episode = 1000
+
+num_episodes = 100
+
+gama = .99
+epsilon = .5
+
+
+# TODO: Better parameters
+def reward(snake):
+    return -1000 if snake.status == Status.DEAD else len(snake.body) * 5
+
+
+cumulative_rewards = []
+try:
+    for i in range(num_episodes):
+        # TODO: Save the model and a game every n-th episodes
+
+        env = SnakeMaze(50, 50, 1)
+        env.reset()
+
+        tot_rew = 0
+        for _ in range(num_step_per_episode):
+
+            if env.num_active_agents is 0:
+                epsilon = 1. / ((i / 50) + 10)
+                break
+
+            state = env.matrix.reshape((1, *env.matrix.shape, 1)).astype('float32')
+            Q = model(state)
+            direction = np.argmax(Q)
+
+            if np.random.rand(1) < epsilon:
+                direction = np.random.randint(4)
+
+            env.step({0: direction})
+            rew = reward(env.snakes[0])
+            tot_rew += rew
+
+            state2 = env.matrix.reshape((1, *env.matrix.shape, 1)).astype('float32')
+            targetQ = model(state2)
+
+            best_Q = np.max(targetQ)
+            targetQ = targetQ.numpy()
+            targetQ[0, direction] = rew + gama * best_Q
+            targetQ = tf.convert_to_tensor(targetQ)
+
+            train_step(state, targetQ)
+
+        print(f"Episode {i} Done!\nEpisode reward: {tot_rew}\n")
+        cumulative_rewards.append(tot_rew)
+except KeyboardInterrupt:
+    pass
+
+print(cumulative_rewards)
+
 env = SnakeMaze(50, 50, 1)
 env.reset()
+renderer = CV2Renderer(env)
 
-# TODO: Refactor the environment to keep float32 values, Train the network
-model = DQN(input_shape=env.matrix.shape)
-inp_image = env.matrix.reshape((1, *env.matrix.shape, 1)).astype('float32')
-print(model(inp_image))
+while env.num_active_agents:
+    renderer.render()
+
+    state = env.matrix.reshape((1, *env.matrix.shape, 1)).astype('float32')
+    d = np.argmax(model(state))
+
+    env.step({0: d})
+    renderer.render()
+
+renderer.destroy_window()
+renderer.save_video('../game_videos')
